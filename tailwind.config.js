9b2b402518c2b265c2d3fd618f75cf6912ec4722
subhayudas <subhayudas49@gmail.com>
2025-01-03 16:01:06 +0530

    small changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,52 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      animation: {
-        gradient: 'gradient 8s linear infinite',
-        blob: 'blob 7s infinite',
-      },
-      keyframes: {
-        gradient: {
-          '0%, 100%': {
-            'background-size': '200% 200%',
-            'background-position': 'left center',
-          },
-          '50%': {
-            'background-size': '200% 200%',
-            'background-position': 'right center',
-          },
-        },
-        blob: {
-          '0%': {
-            transform: 'translate(0px, 0px) scale(1)',
-          },
-          '33%': {
-            transform: 'translate(30px, -50px) scale(1.1)',
-          },
-          '66%': {
-            transform: 'translate(-20px, 20px) scale(0.9)',
-          },
-          '100%': {
-            transform: 'translate(0px, 0px) scale(1)',
-          },
-        },
-      },
-    },
+    extend: {},
   },
-  plugins: [
-    function({ addUtilities }) {
-      addUtilities({
-        '.animation-delay-2000': {
-          'animation-delay': '2s',
-        },
-        '.animation-delay-4000': {
-          'animation-delay': '4s',
-        },
-      })
-    },
-  ],
+  plugins: [],
 }
